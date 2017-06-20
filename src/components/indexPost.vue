134ef84feb29ad8911551fa88cc4db3bc6ba1afc
Victor <victoraweb@gmail.com>
2017-06-20 14:29:36 -0300

    [FEAT] Search filter

--- src/components/indexPost.vue
+++ src/components/indexPost.vue
@@ -2,12 +2,16 @@
     <div>
         <h3>Posts</h3>
 
+        <div id="search-container">
+            <input v-model="search" type="text" placeholder="search for a post...">
+        </div>
+
         <div v-if="errorPresent">
             <h3>Something went wrong</h3>
             {{errors}}
         </div>
         
-        <div v-theme:columns="'narrow'" v-for="post in posts" class="post-container">
+        <div v-theme:columns="'narrow'" v-for="post in filteredPosts" class="post-container">
             <h2 v-rainbow>{{post.title | to-uppercase}}</h2>
             <p>{{post.body | snippet}}</p>
             <p>Id: {{post.id}}</p>
@@ -23,7 +27,15 @@ export default {
         return {
             posts: [],
             errorPresent: false,
-            errors: null
+            errors: null,
+            search: ''
+        }
+    },
+    computed: {
+        filteredPosts: function() {
+            return this.posts.filter((post) => {
+                return post.title.match(this.search);
+            });
         }
     },
     created() {
