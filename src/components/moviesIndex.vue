43b84358c582e1a4e6bee5521e3b72b253448754
Victor <victoraweb@gmail.com>
2017-06-20 17:17:58 -0300

    [FEAT] Movies list displayed

--- src/components/moviesIndex.vue
+++ src/components/moviesIndex.vue
@@ -2,6 +2,9 @@
     <div id="movies-index-container">
         <h1>Movies list</h1>
         <div v-for="movie in movies">
+            <h3>{{movie.title}}</h3>
+            <p>{{movie.description}}</p>
+            <p>Added on {{movie.created_date}}</p>
         </div>
     </div>
 </template>
@@ -16,9 +19,8 @@ export default {
     },
     created() {
         apiMovies.get('/movies')
-        .then(data => {
-            console.log('Movies: ', data);
-            this.movies = data;
+        .then(res => {
+            this.movies = res.data;
         })
         .catch(err => {
             console.log('Error in fetching movies: ', err)
